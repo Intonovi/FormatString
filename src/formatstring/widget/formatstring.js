9e334c7efea7f405d4745d3cdb0c732d523e1974
Petar Vukmanovic <petar.vukmanovic@intonovi.com>
2021-01-19 10:02:15 +0100

    Update for Mx8

--- src/formatstring/widget/formatstring.js
+++ src/formatstring/widget/formatstring.js
@@ -132,17 +132,25 @@ define([
                 return returnEnum === "" ? attrObj.emptyReplacement : returnEnum;
             }
 
-            if (obj.isNumeric(attr) || (obj.isCurrency && obj.isCurrency(attr)) || obj.getAttributeType(attr) === "AutoNumber") {
-                var numberOptions = {};
-                numberOptions.places = attrObj.decimalPrecision;
-                if (attrObj.groupDigits) {
-                    numberOptions.locale = this._getLocale();
-                    numberOptions.groups = true;
-                }
+            /**
+             * 01/19/2020
+             * Petar Vukmanovic <petar.vukmanovic@intonovi.com>
+             * 
+             * Code update to fix deprication of .isCurrency() method for
+             * new Mendix 8
+             * 
+             */
+            // if (obj.isNumeric(attr) || (obj.isCurrency && obj.isCurrency(attr)) || obj.getAttributeType(attr) === "AutoNumber") {
+            //     var numberOptions = {};
+            //     numberOptions.places = attrObj.decimalPrecision;
+            //     if (attrObj.groupDigits) {
+            //         numberOptions.locale = this._getLocale();
+            //         numberOptions.groups = true;
+            //     }
 
-                var returnNumber = mx.parser.formatValue(obj.get(attr), obj.getAttributeType(attr), numberOptions);
-                return returnNumber === "" ? attrObj.emptyReplacement : returnNumber;
-            }
+            //     var returnNumber = mx.parser.formatValue(obj.get(attr), obj.getAttributeType(attr), numberOptions);
+            //     return returnNumber === "" ? attrObj.emptyReplacement : returnNumber;
+            // }
 
             var returnValue = "";
             if (obj.getAttributeType(attr) === "String") {
